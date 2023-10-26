d4ea688265a463b3d9ba25c95d0ee9e1426b9243
Eric Antones <eantones@nuobit.com>
2023-10-26 18:16:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Wordpress Alias URL Stream Plugin
+
+**Version:** 1.0.0  
+**Author:** NuoBiT Solutions, S.L.  
+**License:** AGPLv3
+
+## Description:
+
+Enables server-side mapping of a single file to multiple virtual names without duplicating or renaming the file on the server. Using a custom path '/alias/' to differentiate between the real and virtual parts of a URL, this plugin enhances the flexibility of file URL naming, allowing users to access and download files with any custom name while serving from the original source.
+
+## Core Functionality:
+
+- **Single Source, Multiple Names**: Host a file once on your server, irrespective of its name – whether it's user-friendly or a unique identifier like `dd0588c172986c32636ffdd8cc690de7b41bf253.pdf`. This plugin allows users to virtually access and download that file with any custom name they prefer.
+
+- **Dynamic Virtual Naming**: By using a specified keyword (e.g., "alias"), users can create virtual paths to the original file. For instance, accessing the URL `https://yourwebsite.com/reports/2023/10/dd0588c172986c32636ffdd8cc690de7b41bf253.pdf/alias/test.pdf` will serve the contents of the original `https://yourwebsite.com/reports/2023/10/dd0588c172986c32636ffdd8cc690de7b41bf253.pdf` file but under the new virtual name `test.pdf`.
+
+- **Efficient File Delivery**: There are no redirections or complex server rerouting involved. The plugin efficiently maps the virtual name to the original file, ensuring seamless file delivery.
+
+## Use Cases:
+
+- **Custom File Naming for Different Audiences**: Share the same file with different groups under different names without having to duplicate the file on the server.
+
+- **SEO and Marketing Considerations**: Serve the file under different virtual names for SEO or marketing campaigns while maintaining a single source of truth on the server.
+
+- **Temporary Naming**: For temporary events or promotions, give a file a custom virtual name without altering its original path or name.
+
+## GitHub Repository:
+
+[NuoBiT's Github Repository](https://github.com/nuobit/wp-alias-url-stream)
+
---
+++ wp-alias-url-stream.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Wordpress Alias URL Stream
+Plugin URI: https://github.com/nuobit/wp-alias-url-stream
+Description: Enables server-side mapping of a single file to multiple virtual names without duplicating or renaming the file on the server. Using a virtual path '/alias/' to differentiate between the real and virtual parts of a URL, this plugin enhances the flexibility of file URL naming, allowing users to access and download files with any custom name while serving from the original source.
+Version: 1.0.0
+Author: NuoBiT Solutions, S.L.
+Author URI: https://www.nuobit.com/
+License: AGPLv3 or later
+Text Domain: wp-alias-url-stream
+License URI: https://www.gnu.org/licenses/agpl-3.0.html
+*/
+
+
+define('VIRTUAL_PATH', 'alias');
+
+// Add custom route
+function wp_alias_url_stream_add_custom_route() {
+    add_rewrite_rule('^(.+?)/' . VIRTUAL_PATH . '/([^/]+)$', 'index.php?actual_file_path=$matches[1]&virtual_filename=$matches[2]', 'top');
+}
+add_action('init', 'wp_alias_url_stream_add_custom_route');
+
+
+// Add custom query vars
+function wp_alias_url_stream_add_custom_query_vars($vars) {
+    $vars[] = 'actual_file_path';
+    $vars[] = 'virtual_filename';
+    return $vars;
+}
+add_filter('query_vars', 'wp_alias_url_stream_add_custom_query_vars');
+
+// Disable canonical redirects for our custom path
+function wp_alias_url_stream_disable_redirect_canonical($redirect_url, $requested_url) {
+    // Check if the URL ends with /VIRTUAL_PATH/something
+    if (preg_match('#/' . VIRTUAL_PATH . '/[^/]+$#', $requested_url)) {
+        return false;
+    }
+    return $redirect_url;
+}
+add_filter('redirect_canonical', 'wp_alias_url_stream_disable_redirect_canonical', 10, 2);
+
+// Handle the request and serve the file
+function wp_alias_url_stream_serve_file() {
+    $actual_file = get_query_var('actual_file_path', false);
+    $virtual_filename = get_query_var('virtual_filename', false);
+
+    if ($actual_file && $virtual_filename) {
+        $file_path = ABSPATH . $actual_file;
+        if (file_exists($file_path)) {
+            $file_info = new finfo(FILEINFO_MIME_TYPE);
+            $mime_type = $file_info->file($file_path);
+            header("Content-Type: $mime_type");
+            header("Content-Disposition: inline; filename=\"$virtual_filename\"");
+            readfile($file_path);
+            exit;
+        }
+    }
+}
+add_action('template_redirect', 'wp_alias_url_stream_serve_file');
+
+// Activation hook
+function wp_alias_url_stream_activate() {
+    wp_alias_url_stream_add_custom_route();
+    flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'wp_alias_url_stream_activate');
+
+// Deactivation hook
+function wp_alias_url_stream_deactivate() {
+    flush_rewrite_rules();
+}
+register_deactivation_hook(__FILE__, 'wp_alias_url_stream_deactivate');
